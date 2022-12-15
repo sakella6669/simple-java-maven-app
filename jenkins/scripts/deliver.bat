bed38f0a0ff217258950e168eee9fea94e29969d
Suresh Akella <sakella.6669@gmail.com>
2022-12-15 13:41:55 +0530

    Update deliver.bat

--- jenkins/scripts/deliver.bat
+++ jenkins/scripts/deliver.bat
@@ -1,8 +1,1 @@
-echo 'The following Maven command installs your Maven-built Java application'
-echo 'into the local Maven repository, which will ultimately be stored in'
-echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
-echo 'volume).'
-mvn jar:jar install:install help:evaluate -Dexpression=project.name
-echo 'The following command runs and outputs the execution of your Java'
-echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 java -jar target/my-app-1.0-SNAPSHOT.jar
